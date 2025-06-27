bb6f12e040f974b296fd99e1dc8a7438ad0c0b3d
Christophe Meessen <christophe@meessen.net>
2025-06-27 11:11:42 +0200

    remove Cache3Hit stdmap benchmark

--- stdmap/puremap_test.go
+++ stdmap/puremap_test.go
@@ -82,39 +82,3 @@ func BenchmarkCache2Miss(b *testing.B) {
 		})
 	}
 }
-
-func BenchmarkCache3Hit(b *testing.B) {
-	var size int
-	for i := range cacheSizes {
-		if cacheSizes[i] > size {
-			size = cacheSizes[i]
-		}
-	}
-	ss := make([]string, size)
-	us := make([]string, size)
-	for i := range size {
-		ss[i] = str(i)
-		us[i] = str(i)
-	}
-	for _, size := range cacheSizes {
-		rng := rand.New(rand.NewPCG(fixedSeed1, fixedSeed2))
-		rng.Shuffle(size, func(i, j int) {
-			us[i], us[j] = us[j], us[i]
-		})
-
-		b.Run(fmt.Sprintf("%8d map", size), func(b *testing.B) {
-			m := make(map[string]int, size)
-			for i := range size {
-				m[ss[i]] = i
-			}
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				idx := i % size
-				_, found := m[us[idx]]
-				if !found {
-					b.Fatalf("Key %s should be found", us[idx])
-				}
-			}
-		})
-	}
-}
